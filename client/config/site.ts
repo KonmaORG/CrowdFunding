cc232f8501c9137b8c4c7482ed71f2eab6f13c86
rahat <70068895+youreachedrahat@users.noreply.github.com>
2025-01-27 12:17:02 +0530

    create started

--- client/config/site.ts
+++ client/config/site.ts
@@ -9,20 +9,12 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Create",
+      href: "/create",
     },
     {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
+      label: "Campaigns",
+      href: "/campaigns",
     },
   ],
   navMenuItems: [
