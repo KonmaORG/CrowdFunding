b20a7e49ca4f1e1e3ba11364694c349392aa6c7d
Cryptonean <faizanvit8@gmail.com>
2025-01-28 08:25:35 +0530

    getPID, getAddr

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,3 +1,12 @@
+import { NETWORK } from "@/config";
+import {
+  MintingPolicy,
+  mintingPolicyToId,
+  Script,
+  Validator,
+  validatorToAddress,
+} from "@lucid-evolution/lucid";
+import { Validation } from "@react-types/shared";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -30,4 +39,16 @@ export function handleError(error: any) {
   // type: "error",
   // });
   console.error(failureCause ?? { error });
+}
+
+export function getAddress(validatorFucntion: { (): Validator; (): Script }) {
+  const validator: Validator = validatorFucntion();
+  const address = validatorToAddress(NETWORK, validator);
+  return address;
+}
+
+export function getPolicyId(validatorFucntion: { (): Validator; (): Script }) {
+  const validator: MintingPolicy = validatorFucntion();
+  const Pid = mintingPolicyToId(validator);
+  return Pid;
 }
