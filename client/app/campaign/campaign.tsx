2c61ec7ce450e3f122b44f5682a78b3a93b1b248
Cryptonean <faizanvit8@gmail.com>
2025-01-29 07:50:21 +0530

    remove unnecessary code

--- client/app/campaign/campaign.tsx
+++ client/app/campaign/campaign.tsx
@@ -1,14 +1,8 @@
-import ButtonCreateCampaign from "@/components/campaign/CreateCampaign";
 import { Cambay } from "next/font/google";
 import React from "react";
 
 const campaign = () => {
-  return (
-    <div>
-      <ButtonCreateCampaign />
-      campaign
-    </div>
-  );
+  return <div>campaign</div>;
 };
 
 export default campaign;
