ca2aeeb5148334754de63c694c42f0897a258e8d
Cryptonean <faizanvit8@gmail.com>
2025-02-04 09:44:03 +0530

    contract changes

--- client/config/index.ts
+++ client/config/index.ts
@@ -14,7 +14,7 @@ export const NETWORK: Network = NETWORKx;
 export const PROVIDER: Provider = new Blockfrost(BF_URL, BF_PID);
 
 export const IdetificationPID =
-  "e294cdff07a7d381f3323966d15abfed8a4acd82dd6d762d3a1541d2"; //preview
+  "4d99bb026dafd9ea4f96493ef6e0bd8027e2b518207ff4b4546f88bb"; //preview
 // "93a74c2bd74b872e0e895269626ce4c94f13a45f3743c63e17a8e513"; //emulator
 
 export const PLATFORMADDR =
